df0a3de14fece8920ecdc4603ecc631459968532
code5479 <hgh5479@naver.com>
2022-07-28 12:56:36 +0900

    클래스3

---
+++ src/j09_클래스3/Person.java
@@ -0,0 +1,33 @@
+package j09_클래스3;
+
+public class Person {
+	
+	private String name;       //멤버변수
+	private int age;
+	
+	public Person() {
+		
+	}
+	
+	
+	
+	public Person(String name, int age) {		//생성자의 특징 클래스랑 이름이 동일하고 반환자료가 없다. 매개변수가 다른걸 오버로드
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void test() {
+		String name = "김준일";        //우선순위는 매개변수 = 지역변수가 높음
+		System.out.println(name);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
